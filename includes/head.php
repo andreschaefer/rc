bda60f06c7bc8a491297eef7707531e7209cb75f
Andre Schaefer <andre.schaefer@namics.com>
2015-01-20 21:13:25 +0100

    new css version

--- includes/head.php
+++ includes/head.php
@@ -13,7 +13,7 @@
     <!-- Bootstrap -->
     <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <!--<link href="/bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">-->
-    <link href="/css/rc.css" rel="stylesheet">
+    <link href="/css/rc-20150120.css" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
