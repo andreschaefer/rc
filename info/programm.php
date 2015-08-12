d64190b835b2eea32f6dfd352ce8bc0017b61b3b
Andre Schaefer <andre.schaefer@namics.com>
2015-08-12 17:43:46 +0200

    updates

--- info/programm.php
+++ info/programm.php
@@ -6,7 +6,7 @@
     <div class="col-md-8 info">
         <script type="application/javascript">
 
-            var format = ".docx";
+            var format = ".pdf";
 
             var target = (''+window.location).substr(0, (''+window.location).lastIndexOf(".")) + format;
             var frame = '<iframe src="http://docs.google.com/viewer?url=' + target + '&embedded=true" style="width:100%; height:1060px;" frameborder="0"></iframe>';
