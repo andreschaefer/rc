8e32440304aac55c76ae8499ad8942258c6ce804
Andre Schaefer <andre.schaefer@namics.com>
2015-09-12 13:09:11 +0200

    updates

---
+++ anlass/indoorklettern15.php
@@ -0,0 +1,19 @@
+<?php include '../includes/head.php'; ?>
+<h1>
+	Indoor Klettern
+	<br/> <small>7. oder 21. November 2015</small>
+</h1>
+<div class="row">
+	<div class="col-md-8 info">
+		<script type="application/javascript">
+
+			var format = ".pdf";
+
+			var target = (''+window.location).substr(0, (''+window.location).lastIndexOf(".")) + format;
+			var frame = '<iframe src="http://docs.google.com/viewer?url=' + target + '&embedded=true" style="width:100%; height:1060px;" frameborder="0"></iframe>';
+			document.write(frame);
+		</script>
+	</div>
+</div>
+<?php include '../includes/foot.php'; ?>
+
--- index.php
+++ index.php
@@ -1,14 +1,35 @@
 <?php include 'includes/head.php'; ?>
-    <div class="jumbotron">
+    <div class="jumbotron hidden-xs hidden-sm">
         <h1><img src="/images/rc-logo-100.gif"/> Das Rettungs-Corps</h1>
 
-        <p>Das Rettungs-Corps der Stadt St. Gallen wurde 1859 gegründet, mit dem Zweck, die Pflege und Förderung der
+        <p >Das Rettungs-Corps der Stadt St. Gallen wurde 1859 gegründet, mit dem Zweck, die Pflege und Förderung der
             Kameradschaft unter den
             Aktiven und Mitgliedern der "Alten Garde" sowie Durchführung repräsentativer Anlässe. Im Weiteren wird
             in Not geratenen
             Mitgliedern Hilfe geleistet.</p>
     </div>
-
+    <div class="visible-sm">
+        <h1><img src="/images/rc-logo-50.gif"/> Das Rettungs-Corps St. Gallen</h1>
+    </div>
+    <div class="visible-xs">
+        <h1><img src="/images/rc-logo-50.gif"/> RC St. Gallen</h1>
+    </div>
+<!-- FIXME remove hard coded teaser!-->
+    <div class="row">
+        <div class="col-md-8">
+            <img class="img-responsive" src="images/2015-09-12-degersheimer.jpg" />
+        </div>
+        <div class="col-md-4">
+            <h2>Degersheihmer Feuerwehrmarsch <br/>
+                <small>Samstag, 12. September 2015</small>
+            </h2>
+            <p>Viele liebe Grüsse aus Degersheim!</p>
+            <p>
+                Bei schönstem Wetter startet die RC-Delegation am Degersheimer Feuerwehrmarsch.
+            </p>
+            <p>Sei nächstes Jahr auch dabei an einem immer wieder schönen Anlass!</p>
+        </div>
+    </div>
 <?php
 $files = get_files('teaser', '.html');
 $count = 0;
