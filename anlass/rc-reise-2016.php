2c7feb63a647062c39c24f8177638fba85eeefd4
Andre Schaefer <andre.schaefer@namics.com>
2016-03-26 12:56:34 +0100

    reise infos

---
+++ anlass/rc-reise-2016.php
@@ -0,0 +1,46 @@
+<?php include '../includes/head.php'; ?>
+<h1>RC-Reise mit dem Apfelsaft-Express<br/>
+    <small>Samstag, 4. Juni 2016</small>
+</h1>
+
+<div class="row">
+    <div class="col-md-4">
+        <img class="img-responsive" src="images/ausflug-1.png" />
+    </div>
+    <div class="col-md-4">
+        <img class="img-responsive" src="images/ausflug-2.png" />
+    </div>
+    <div class="col-md-4">
+        <img class="img-responsive" src="images/ausflug-3.png" />
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-4">
+        <img class="img-responsive" src="images/ausflug-4.png" />
+    </div>
+    <div class="col-md-4">
+        <img class="img-responsive" src="images/ausflug-5.png" />
+    </div>
+    <div class="col-md-4">
+        <img class="img-responsive" src="images/ausflug-6.png" />
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-4">
+        <img class="img-responsive" src="images/ausflug-7.png" />
+    </div>
+</div>
+
+<!--<div class="row">-->
+<!--    <div class="col-md-8 info">-->
+<!--        <script type="application/javascript">-->
+<!---->
+<!--            var format = ".pdf";-->
+<!---->
+<!--            var target = (''+window.location).substr(0, (''+window.location).lastIndexOf(".")) + format;-->
+<!--            var frame = '<iframe src="http://docs.google.com/viewer?url=' + target + '&embedded=true" style="width:100%; height:1060px;" frameborder="0"></iframe>';-->
+<!--            document.write(frame);-->
+<!--        </script>-->
+<!--    </div>-->
+<!--</div>-->
+<?php include '../includes/foot.php'; ?>
