77e3edcae317526a4ab0857c073f00561983cfce
Andre Schaefer <andre.schaefer@namics.com>
2016-03-29 16:46:56 +0200

    rc reise

--- anlass/rc-reise-2016.php
+++ anlass/rc-reise-2016.php
@@ -3,6 +3,20 @@
     <small>Samstag, 4. Juni 2016</small>
 </h1>
 
+
+<div class="row">
+    <div class="col-md-8 info">
+        <script type="application/javascript">
+
+            var format = ".pdf";
+
+            var target = (''+window.location).substr(0, (''+window.location).lastIndexOf(".")) + format;
+            var frame = '<iframe src="http://docs.google.com/viewer?url=' + target + '&embedded=true" style="width:100%; height:1060px;" frameborder="0"></iframe>';
+            document.write(frame);
+        </script>
+    </div>
+</div>
+
 <div class="row">
     <div class="col-md-4">
         <img class="img-responsive" src="/images/ausflug-1.png" />
@@ -31,16 +45,4 @@
     </div>
 </div>
 
-<!--<div class="row">-->
-<!--    <div class="col-md-8 info">-->
-<!--        <script type="application/javascript">-->
-<!---->
-<!--            var format = ".pdf";-->
-<!---->
-<!--            var target = (''+window.location).substr(0, (''+window.location).lastIndexOf(".")) + format;-->
-<!--            var frame = '<iframe src="http://docs.google.com/viewer?url=' + target + '&embedded=true" style="width:100%; height:1060px;" frameborder="0"></iframe>';-->
-<!--            document.write(frame);-->
-<!--        </script>-->
-<!--    </div>-->
-<!--</div>-->
 <?php include '../includes/foot.php'; ?>
