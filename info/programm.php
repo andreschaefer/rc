21df6b972d313ce1f2bdfb621cf0afc0604a5456
Andre Schaefer <andre.schaefer@namics.com>
2016-03-29 16:35:56 +0200

    reise infos

--- info/programm.php
+++ info/programm.php
@@ -1,12 +1,12 @@
 <?php include '../includes/head.php'; ?>
 <h1>Tätigkeitsprogramm des RC<br/>
-    <small>2015</small>
+    <small>2016</small>
 </h1>
 <div class="row">
     <div class="col-md-8 info">
         <script type="application/javascript">
 
-            var format = ".pdf";
+            var format = ".docx";
 
             var target = (''+window.location).substr(0, (''+window.location).lastIndexOf(".")) + format;
             var frame = '<iframe src="http://docs.google.com/viewer?url=' + target + '&embedded=true" style="width:100%; height:1060px;" frameborder="0"></iframe>';
