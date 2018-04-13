f6f10f230266a0bc88420372d008a8ad8ea192da
Andre Schaefer <andre.schaefer@namics.com>
2018-04-13 19:56:40 +0200

    update

---
+++ anlass/statuten-versammlung.php
@@ -0,0 +1,10 @@
+<?php include '../includes/head.php'; ?>
+<h1>Vorstellung der neuen Statuten<br/>
+	<small>27. April 2018</small>
+</h1>
+<div class="row">
+	<div class="col-md-8 info">
+		<img src="statuten-versammlung.png">
+	</div>
+</div>
+<?php include '../includes/foot.php'; ?>
