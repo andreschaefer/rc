4bd52948691bb80f9a0285dd7a179d1365b05fdd
Andre Schaefer <andre.schaefer@namics.com>
2018-04-20 18:11:20 +0200

    update

--- anlass/statuten-versammlung.php
+++ anlass/statuten-versammlung.php
@@ -1,10 +1,10 @@
 <?php include '../includes/head.php'; ?>
-<h1>Vorstellung der neuen Statuten<br/>
+<h1>Rückruf Versammlung<br/>
 	<small>27. April 2018</small>
 </h1>
 <div class="row">
 	<div class="col-md-8 info">
-		<img src="statuten-versammlung.png" width="100%">
+		<img src="statuten-versammlung-rueckruf.png" width="100%">
 	</div>
 </div>
 <?php include '../includes/foot.php'; ?>
