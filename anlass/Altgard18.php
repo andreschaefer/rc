80859c971c7214997edfeb8735ffc283b09dd790
aschaefer <andre.schaefer@namics.com>
2018-05-15 10:37:25 +0200

    Update altagrdistentreffen 18

---
+++ anlass/Altgard18.php
@@ -0,0 +1,17 @@
+<?php include '../includes/head.php'; ?>
+<h1>Altgardistentreffen<br/>
+    <small>Samstag 2. Juni 2018</small>
+</h1>
+<div class="row">
+    <div class="col-md-8 info">
+        <script type="application/javascript">
+
+            var format = ".doc";
+
+			var target = (''+window.location).substr(0, (''+window.location).lastIndexOf(".")) + format;
+			var frame = '<iframe src="http://docs.google.com/viewer?url=' + target + '&embedded=true" style="width:100%; height:1060px;" frameborder="0"></iframe>';
+			document.write(frame);
+        </script>
+    </div>
+</div>
+<?php include '../includes/foot.php'; ?>
