4d6f334f1dfd3699d55b59ad3bb6cf72d021c6b9
aschaefer <andre.schaefer@namics.com>
2018-05-15 16:48:17 +0200

    use https urls for google doc viewer

--- anlass/10Kampf_15.php
+++ anlass/10Kampf_15.php
@@ -9,7 +9,7 @@
             var format = ".pdf";
 
             var target = (''+window.location).substr(0, (''+window.location).lastIndexOf(".")) + format;
-            var frame = '<iframe src="http://docs.google.com/viewer?url=' + target + '&embedded=true" style="width:100%; height:1060px;" frameborder="0"></iframe>';
+            var frame = '<iframe src="https://docs.google.com/viewer?url=' + target + '&embedded=true" style="width:100%; height:1060px;" frameborder="0"></iframe>';
             document.write(frame);
         </script>
     </div>
