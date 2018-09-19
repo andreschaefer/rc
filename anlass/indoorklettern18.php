4ce0ec44b9ce0aa27ea7b418669ac9dcc7b45362
Andre Schaefer <andre.schaefer@namics.com>
2018-09-19 22:11:18 +0200

    update

---
+++ anlass/indoorklettern18.php
@@ -0,0 +1,19 @@
+<?php include '../includes/head.php'; ?>
+<h1>
+	Indoor Klettern
+	<br/> <small>17. November 2018</small>
+</h1>
+<div class="row">
+	<div class="col-md-8 info">
+		<script type="application/javascript">
+
+			var format = ".docx";
+
+			var target = (''+window.location).substr(0, (''+window.location).lastIndexOf(".")) + format;
+			var frame = '<iframe src="https://docs.google.com/viewer?url=' + target + '&embedded=true" style="width:100%; height:1060px;" frameborder="0"></iframe>';
+			document.write(frame);
+		</script>
+	</div>
+</div>
+<?php include '../includes/foot.php'; ?>
+
