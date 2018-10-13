437f984a7dbd3a4f7370c54596b27b9218e0bf02
Andre Schaefer <andre.schaefer@namics.com>
2018-10-13 10:19:18 +0200

    statuten

--- anlass/statuten-versammlung.php
+++ anlass/statuten-versammlung.php
@@ -13,10 +13,10 @@
 		Es stehen folgende Dokumente zur Diskussion.
 	</p>
 	<ul>
-		<li><a href="RC_Statutenrevision_Statuten neu alt.docx">Statuten Gegenüberstellung Alt und Neu </a></li>
-		<li><a href="Anlagereglement_final.docx">Anlagereglement</a></li>
-		<li><a href="Reglement Vereinskasse_final.docx">Reglement für die Vereinskasse</a></li>
-		<li><a href="Vereinsheim Noerdli_final.docx">Reglement über das Vereinsheim Nördli</a></li>
+		<li><a href="/anlass/statuten/RC_Statutenrevision_Statuten neu alt.docx">Statuten Gegenüberstellung Alt und Neu </a></li>
+		<li><a href="/anlass/statuten/Anlagereglement_final.docx">Anlagereglement</a></li>
+		<li><a href="/anlass/statuten/Reglement Vereinskasse_final.docx">Reglement für die Vereinskasse</a></li>
+		<li><a href="/anlass/statuten/Vereinsheim Noerdli_final.docx">Reglement über das Vereinsheim Nördli</a></li>
 	</ul>
 
 
