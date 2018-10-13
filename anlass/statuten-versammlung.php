36378737a957ad8d62695e71fe07360e765d77c2
Andre Schaefer <andre.schaefer@namics.com>
2018-10-13 09:15:39 +0200

    add statuten info

--- anlass/statuten-versammlung.php
+++ anlass/statuten-versammlung.php
@@ -1,10 +1,31 @@
 <?php include '../includes/head.php'; ?>
-<h1>Rückruf Versammlung<br/>
-	<small>27. April 2018</small>
-</h1>
+<div class="row">
+	<h1>Ausserordentliche Mitgliedervesammlung - Statuten<br>
+		<small>Montag 22. Oktober 2018</small>
+	</h1>
+	<p>
+		Liebe RC-Mitglieder
+	</p>
+	<p>
+		Die an der HV angekündigte und verschobene ausserordentliche Versammlung zur Vorstellung der neuen Statuten
+		findet am Montag, den 22. Oktober 2018 um 19:30 Uhr im Depot61 (Burgstrasse 61) statt.
+	</p>
+</div>
 <div class="row">
 	<div class="col-md-8 info">
-		<img src="statuten-versammlung-rueckruf.png" width="100%">
+		<img src="statuten-versammlung.png" width="100%">
 	</div>
+</div>
+<div class="row">
+	<h2>Statuten</h2>
+	<p>
+		Es stehen folgende Dokumente zur Diskussion.
+	</p>
+	<a href="RC_Statutenrevision_Statuten neu alt.docx">Statuten Gegenüberstellung Alt und Neu </a>
+	<a href="Anlagereglement_final.docx">Anlagereglement</a>
+	<a href="Reglement Vereinskasse_final.docx">Reglement für die Vereinskasse</a>
+	<a href="Vereinsheim Noerdli_final.docx">Reglement über das Vereinsheim Nördli</a>
+
+
 </div>
 <?php include '../includes/foot.php'; ?>
