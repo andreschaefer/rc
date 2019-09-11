ade689c32d403e41ff7ef54964ad98c618cd2393
Andre Schaefer <andre.schaefer@namics.com>
2019-09-11 10:02:56 +0200

    metgete detail

---
+++ anlass/metzgete19.php
@@ -0,0 +1,18 @@
+<?php include '../includes/head.php'; ?>
+<h1>
+	Nördli Metzgete 
+	<br/> <small>25/26. Oktober 2019</small>
+</h1>
+<div class="row">
+	<div class="col-md-8 info">
+		<script type="application/javascript">
+
+			var format = ".pdf";
+
+			var target = (''+window.location).substr(0, (''+window.location).lastIndexOf(".")) + format;
+			var frame = '<iframe src="https://docs.google.com/viewer?url=' + target + '&embedded=true" style="width:100%; height:1060px;" frameborder="0"></iframe>';
+			document.write(frame);
+		</script>
+	</div>
+</div>
+<?php include '../includes/foot.php'; ?>
