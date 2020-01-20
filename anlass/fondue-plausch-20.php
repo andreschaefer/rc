54821e96f07ef87859967eec41677655bc820669
Andre Schaefer <andre.schaefer@namics.com>
2020-01-20 19:19:27 +0100

    fondue 20

---
+++ anlass/fondue-plausch-20.php
@@ -0,0 +1,17 @@
+<?php include '../includes/head.php'; ?>
+<h1>Fondue-Plausch <br/>
+    <small>21. und 22. Februar 2020</small>
+</h1>
+<div class="row">
+    <div class="col-md-8 info">
+        <script type="application/javascript">
+
+            var format = ".pdf";
+
+            var target = (''+window.location).substr(0, (''+window.location).lastIndexOf(".")) + format;
+            var frame = '<iframe src="https://docs.google.com/viewer?url=' + target + '&embedded=true" style="width:100%; height:1060px;" frameborder="0"></iframe>';
+            document.write(frame);
+        </script>
+    </div>
+</div>
+<?php include '../includes/foot.php'; ?>
