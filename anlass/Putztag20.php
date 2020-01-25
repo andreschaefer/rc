96c0e5c105e5b00ffee0f2d61df770259f278411
Andre Schaefer <andre.schaefer@namics.com>
2020-01-25 11:44:20 +0100

    putztag 20

---
+++ anlass/Putztag20.php
@@ -0,0 +1,17 @@
+<?php include '../includes/head.php'; ?>
+<h2>Nördli-Putztag <br/>
+    <small>Samstag 4. April 2020</small>
+</h2>
+<div class="row">
+    <div class="col-md-8 info">
+        <script type="application/javascript">
+
+            var format = ".pdf";
+
+            var target = (''+window.location).substr(0, (''+window.location).lastIndexOf(".")) + format;
+            var frame = '<iframe src="https://docs.google.com/viewer?url=' + target + '&embedded=true" style="width:100%; height:1060px;" frameborder="0"></iframe>';
+            document.write(frame);
+        </script>
+    </div>
+</div>
+<?php include '../includes/foot.php'; ?>
