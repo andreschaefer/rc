55f10d5c71a3b9c9624419b9e979bb042e03e9c4
aschaefer <andre.schaefer@namics.com>
2020-04-30 23:07:33 +0200

    vorbereitung hv

---
+++ hv2020/index.php
@@ -0,0 +1,8 @@
+<?php include '../includes/head.php'; ?>
+<h1>Hauptversammlung 2020</h1>
+<div class="row">
+	<div class="col-md-8 info">
+
+	</div>
+</div>
+<?php include '../includes/foot.php'; ?>
