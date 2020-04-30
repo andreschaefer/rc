4acafbd434cb9637caaee7aa5ca1433b34dc8881
aschaefer <andre.schaefer@namics.com>
2020-04-30 23:56:10 +0200

    vorbereitung hv

--- hv2020/index.php
+++ hv2020/index.php
@@ -1,7 +1,69 @@
 <?php include '../includes/head.php'; ?>
-<h1>Hauptversammlung 2020</h1>
+<h1>Schriftliche Hauptversammlung 2020<br>
+<small>Ersatz für die Hauptversammlung vom Freitag 27. März 2020</small></h1>
 <div class="row">
 	<div class="col-md-8 info">
+		<h2>Liebe RC-Mitglieder</h2>
+		<p>
+			Die aktuelle Situation lässt immer noch keine Hauptversammlung zu und so wie es sich abzeichnet, wäre eine Hauptversammlung in diesem Jahr
+			unverantwortlich, da mehr als die Hälfte der Mitglieder zu einer Risikogruppe gehören. Ohne Hauptversammung können wir aber die
+			ordentlichen Geschäfte, vor allem die Kasse, nicht rechtmässig weiterführen. Aus diesem Grund hat sich die Kommission entschieden, die
+			Hauptversammlung 2020 auf schriftlichem Wege durchzuführen.
+		</p>
+		<p>
+			Unten findet Ihr die Traktanden mit zusätzlichen Informationen. Auf den auf farbigem Papier gedruckten Zetteln findet Ihr die
+			entsprechenden Abstimmungstexte, wo Ihr Ja oder Nein ankreuzt. Bei den Wahlen findet Ihr zwei Versionen (blau und grün hinterlegt), bei
+			denen je nach Abstimmungsresultat bei Traktandum 7 - Statutenänderung, die Abstimmungstexte leicht anders lauten. Bitte füllt beide
+			Versionen aus.
+		</p>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-8 info">
+		<h2>Dokumente zu den einzelnen Traktanden</h2>
+
+		<h3>3. Protokoll der ordentlichen Hauptversammlung vom 29. März 2019</h3>
+
+		<a href="traktandum03/2019-03-29_HV_Protokoll.pdf">Protokoll der HV vom 29.3.2019</a>
+		<a href="traktandum03/2019_Jahresbericht_Obmann.pdf">Jahresbericht des Obmann 2018</a>
+		<a href="traktandum03/2019_noerdli_bericht.pdf">Nördli Hüttenbericht 2018</a>
+
+		<h3>4. Jahresbericht des Obmannes</h3>
+		<!-- TODO --><strong>TODO</strong>
+		<a href="traktandum04/TODO.pdf">Jahresbericht des Obmannes 2019</a>
+
+		<h3>5. Rechnungsablagen</h3>
+		<!-- TODO --><strong>TODO</strong>
+		<a href="traktandum05/UK.pdf">Unterstützungskasse</a>
+		<a href="traktandum05/CK.pdf">Corpskasse</a>
+		<a href="traktandum05/HK.pdf">Hüttenkasse Nördli</a>
+
+
+		<h3>6. Bericht der Geschäftsprüfungskommission</h3>
+		<!-- TODO --><strong>TODO</strong>
+		<a href="traktandum06/TODO.pdf">Bericht der GKP</a>
+
+		<h3>7. Statutenänderung</h3>
+
+		<a href="traktandum07/Neuerungen.pdf">Zusammenfassung der Neuerungen</a>
+		<a href="traktandum07/Statuten_neu_alt.pdf">Gegenüberstellung der neuen und alten Statuten</a>
+
+		<h4>Endfassung zur Abstimmung</h4>
+
+		<a href="traktandum07/Statuten.pdf">Statuten</a>
+		<a href="traktandum07/Vereinskasse.pdf">Reglement über die Vereinskasse</a>
+		<a href="traktandum07/Anlagereglement.pdf">Anlagereglement</a>
+		<a href="traktandum07/NoerdliReglement.pdf">Reglement über das Vereinsheim Nördli</a>
+
+		<h3>10. Vereinsheim Nördli</h3>
+
+		<a href="traktandum10/2020_noerdli_bericht.pdf">Nördli Hüttenbericht 2019</a>
+		<a href="traktandum10/2020_noerdli_taxen.pdf">Benützungstaxen Nördli</a>
+
+		<h3>14. Vereinsheim Nördli</h3>
+
+		<!-- TODO --><strong>TODO</strong>
+		<a href="traktandum14/TODO.pdf">Antrag Erneuerung Küche im Nördli</a>
 
 	</div>
 </div>
