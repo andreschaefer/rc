9a625f5cf9c2bfc2e2f4b1d639f4764cdf026d0d
aschaefer <andre.schaefer@namics.com>
2020-05-01 00:01:35 +0200

    vorbereitung hv

--- hv2020/index.php
+++ hv2020/index.php
@@ -1,6 +1,6 @@
 <?php include '../includes/head.php'; ?>
 <h1>Schriftliche Hauptversammlung 2020<br>
-<small>Ersatz für die Hauptversammlung vom Freitag 27. März 2020</small></h1>
+	<small>Ersatz für die Hauptversammlung vom Freitag 27. März 2020</small></h1>
 <div class="row">
 	<div class="col-md-8 info">
 		<h2>Liebe RC-Mitglieder</h2>
@@ -18,53 +18,64 @@
 		</p>
 	</div>
 </div>
-<div class="row">
-	<div class="col-md-8 info">
-		<h2>Dokumente zu den einzelnen Traktanden</h2>
+<h2>Dokumente zu den einzelnen Traktanden</h2>
 
-		<h3>3. Protokoll der ordentlichen Hauptversammlung vom 29. März 2019</h3>
+<h3>3. Protokoll der ordentlichen Hauptversammlung vom 29. März 2019</h3>
 
-		<a href="traktandum03/2019-03-29_HV_Protokoll.pdf">Protokoll der HV vom 29.3.2019</a>
-		<a href="traktandum03/2019_Jahresbericht_Obmann.pdf">Jahresbericht des Obmann 2018</a>
-		<a href="traktandum03/2019_noerdli_bericht.pdf">Nördli Hüttenbericht 2018</a>
+<ul>
+	<li><a href="traktandum03/2019-03-29_HV_Protokoll.pdf">Protokoll der HV vom 29.3.2019</a></li>
+	<li><a href="traktandum03/2019_Jahresbericht_Obmann.pdf">Jahresbericht des Obmann 2018</a></li>
+	<li><a href="traktandum03/2019_noerdli_bericht.pdf">Nördli Hüttenbericht 2018</a></li>
+</ul>
 
-		<h3>4. Jahresbericht des Obmannes</h3>
-		<!-- TODO --><strong>TODO</strong>
-		<a href="traktandum04/TODO.pdf">Jahresbericht des Obmannes 2019</a>
+<h3>4. Jahresbericht des Obmannes</h3>
+<!-- TODO --><strong>TODO</strong>
+<ul>
 
-		<h3>5. Rechnungsablagen</h3>
-		<!-- TODO --><strong>TODO</strong>
-		<a href="traktandum05/UK.pdf">Unterstützungskasse</a>
-		<a href="traktandum05/CK.pdf">Corpskasse</a>
-		<a href="traktandum05/HK.pdf">Hüttenkasse Nördli</a>
+	<li><a href="traktandum04/TODO.pdf">Jahresbericht des Obmannes 2019</a></li>
+</ul>
+
+<h3>5. Rechnungsablagen</h3>
+<!-- TODO --><strong>TODO</strong>
+<ul>
+	<li><a href="traktandum05/UK.pdf">Unterstützungskasse</a></li>
+	<li><a href="traktandum05/CK.pdf">Corpskasse</a></li>
+	<li><a href="traktandum05/HK.pdf">Hüttenkasse Nördli</a></li>
+</ul>
 
 
-		<h3>6. Bericht der Geschäftsprüfungskommission</h3>
-		<!-- TODO --><strong>TODO</strong>
-		<a href="traktandum06/TODO.pdf">Bericht der GKP</a>
+<h3>6. Bericht der Geschäftsprüfungskommission</h3>
+<!-- TODO --><strong>TODO</strong>
+<ul>
+	<li><a href="traktandum06/TODO.pdf">Bericht der GKP</a></li>
+</ul>
 
-		<h3>7. Statutenänderung</h3>
+<h3>7. Statutenänderung</h3>
 
-		<a href="traktandum07/Neuerungen.pdf">Zusammenfassung der Neuerungen</a>
-		<a href="traktandum07/Statuten_neu_alt.pdf">Gegenüberstellung der neuen und alten Statuten</a>
+<ul>
+	<li><a href="traktandum07/Neuerungen.pdf">Zusammenfassung der Neuerungen</a></li>
+	<li><a href="traktandum07/Statuten_neu_alt.pdf">Gegenüberstellung der neuen und alten Statuten</a></li>
+</ul>
 
-		<h4>Endfassung zur Abstimmung</h4>
+<h4>Endfassung zur Abstimmung</h4>
 
-		<a href="traktandum07/Statuten.pdf">Statuten</a>
-		<a href="traktandum07/Vereinskasse.pdf">Reglement über die Vereinskasse</a>
-		<a href="traktandum07/Anlagereglement.pdf">Anlagereglement</a>
-		<a href="traktandum07/NoerdliReglement.pdf">Reglement über das Vereinsheim Nördli</a>
+<ul>
+	<li><a href="traktandum07/Statuten.pdf">Statuten</a></li>
+	<li><a href="traktandum07/Vereinskasse.pdf">Reglement über die Vereinskasse</a></li>
+	<li><a href="traktandum07/Anlagereglement.pdf">Anlagereglement</a></li>
+	<li><a href="traktandum07/NoerdliReglement.pdf">Reglement über das Vereinsheim Nördli</a></li>
+</ul>
 
-		<h3>10. Vereinsheim Nördli</h3>
+<h3>10. Vereinsheim Nördli</h3>
+<ul>
+<li><a href="traktandum10/2020_noerdli_bericht.pdf">Nördli Hüttenbericht 2019</a></li>
+<li><a href="traktandum10/2020_noerdli_taxen.pdf">Benützungstaxen Nördli</a></li>
+</ul>
 
-		<a href="traktandum10/2020_noerdli_bericht.pdf">Nördli Hüttenbericht 2019</a>
-		<a href="traktandum10/2020_noerdli_taxen.pdf">Benützungstaxen Nördli</a>
+<h3>14. Vereinsheim Nördli</h3>
+<!-- TODO --><strong>TODO</strong>
+<ul>
+	<li><a href="traktandum14/TODO.pdf">Antrag Erneuerung Küche im Nördli</a></li>
+</ul>
 
-		<h3>14. Vereinsheim Nördli</h3>
-
-		<!-- TODO --><strong>TODO</strong>
-		<a href="traktandum14/TODO.pdf">Antrag Erneuerung Küche im Nördli</a>
-
-	</div>
-</div>
 <?php include '../includes/foot.php'; ?>
