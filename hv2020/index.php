42b1232f7a27af446b189dca9067020a4b8df573
aschaefer <andre.schaefer@namics.com>
2020-08-23 11:41:23 +0200

    hv2020 ergebnis

--- hv2020/index.php
+++ hv2020/index.php
@@ -4,6 +4,23 @@
 		<small>Ersatz für die Hauptversammlung vom Freitag 27. März 2020</small></h1>
 	<div class="row">
 		<div class="col-md-8 info">
+			<h2>Ergebniss der schriftlichen Hauptversammlung</h2>
+			<h3>Liebe RC-Mitglieder</h3>
+			<p>
+				Die Ergebnisse der schriftlichen Hauptversammlung 2020 liegen nun vor. Alle Anträge und Abstimmungen wurden mit deutlicher Mehrheit angenommen.
+				Wir danken allen Mitgliedern, die trotz der Umstände zahlreich ihre Stimmen abgegeben haben.
+			</p>
+			<p>
+				Insbesondere möchte sich der neu gewählte Vorstand bei der GPK bedanken, welche diese Form der Haputversammlung als neutrale Stimmauszähler ers ernöglicht haben.
+			</p>
+			<p>
+				Anbei das <li><a href="protokoll-hv2020.pdf">Protokoll schriftlichen HV 2020</a></li>
+			</p>
+			<p>Die vollständigen Unterlagen zur Auszählung werden archiviert und können auf Anfrage eingesehen werden.</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-8 info">
 			<h2>Liebe RC-Mitglieder</h2>
 			<p>
 				Die aktuelle Situation lässt immer noch keine Hauptversammlung zu und so wie es sich abzeichnet, wäre eine Hauptversammlung in diesem
