fd20b8c617e14153eb92489e91657928d0dbed2c
aschaefer <andre.schaefer@namics.com>
2021-04-25 20:43:39 +0200

    update

---
+++ hv2021/index.php
@@ -0,0 +1,27 @@
+<?php include '../includes/head.php'; ?>
+<main class="hv2020">
+	<h1>Schriftliche Hauptversammlung 2021<br>
+	<div class="row">
+		<div class="col-md-8 info">
+			<h2>Ergebniss der schriftlichen Hauptversammlung</h2>
+			<h3>Liebe RC-Mitglieder</h3>
+			<p>
+				Die Ergebnisse der schriftlichen Hauptversammlung 2021 liegen nun vor. Alle Anträge, Abstimmungen und Wahlen wurden mit deutlicher Mehrheit angenommen.
+			<br>
+				Neu zum Obmann gewählt ist André Schäfer (Zug 4)
+			<br>
+				Neu zum RC Kassier gewählt ist Rolf Bill (Altgardist)
+			<br>
+			<p>
+				Wir danken allen Mitgliedern, die trotz der Umstände zahlreich ihre Stimmen abgegeben haben.
+				Insbesondere möchte sich der neu gewählte Vorstand bei der GPK bedanken,
+				welche diese Form der Hauptversammlung als neutrale Stimmenzähler erst ermöglicht haben.
+			</p>
+			<p>
+				Anbei das <li><a href="protokoll-hv2021.pdf">Protokoll schriftlichen HV 2021</a></li>
+			</p>
+			<p>Die vollständigen Unterlagen zur Auszählung werden archiviert und können auf Anfrage eingesehen werden.</p>
+		</div>
+	</div>
+</main>
+<?php include '../includes/foot.php'; ?>
