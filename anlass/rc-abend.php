26875e7d9ee8d4b3f56946cf8b0a70f6621423c4
aschaefer <andre.schaefer@namics.com>
2021-08-23 19:31:12 +0200

    rc abend

---
+++ anlass/rc-abend.php
@@ -0,0 +1,48 @@
+<?php include '../includes/head.php'; ?>
+<h1>Kameradschaftlicher Abend<br/>
+	<small>Freitag, 1. Oktober 2021</small>
+</h1>
+
+<div class="row">
+	<div class="col-md-8">
+		Liebe RC Mitglieder
+		<p>
+			Wir blicken auf schwierige Zeiten zurück und sehen langsam das Licht am Ende des Tunnels.
+			Auch im RC mussten wir in dieser Zeit auf viele traditionelle Anlässe verzichten.
+			Die letzen beiden Hauptversammlungen mussten wir schriftlich durchführen. Für die Formalitäten mag dieses
+			Format genügen, aber die Kameradschaft bleibt dabei natürlich auf der Strecke. Wir konnten zurücktretende
+			Vorstandmitglieder nicht gebührend Danken und werdende Altagardisten nicht würdig ehren. Nicht zuletzt
+			mussten wir auch auf das karadschaftliche Beisammensein verzichten.
+		</p>
+		<p>
+			Aus diesem Grund haben wir im Vorstand beschlossen, euch zu einem gemeinsamen Abend einzuladen. Wir möchten
+			mit euch Ehrungen nachholen und unseren Neumitgliedern die Gelegenheit geben, sich im golden Buch
+			einzutragen. Nicht zu letzt möchten wir mit euch bei einem feinen Essen die Kameradschaft pfelgen, die in
+			letzer Zeit zu kurz gekommen ist.
+		</p>
+
+		<dl>
+			<dd>Wann</dd>
+			<dt>1. Oktober 2021 ab 19:00 Uhr</dt>
+			<dd>Wo</dd>
+			<dt>Feuerwehrmuseum Depot61</dt>
+		</dl>
+
+		<h3>Anmeldungen</h3>
+
+		<p>
+			Damit wir den Abend planen können, bitten wir euch um eure Anmeldung bis zum 26. September 2021.
+		</p>
+		<dl>
+			<dd>Online</dd>
+			<dt>https://forms.gle/izXMBWN1VFCuWshL8</dt>
+		</dl>
+		<p>
+			Wir freuen uns auf einen gemütlichen Abend mit euch!
+		</p>
+		<p>
+			Euer RC Vorstand
+		</p>
+	</div>
+</div>
+<?php include '../includes/foot.php'; ?>
