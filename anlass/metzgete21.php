88e6e4cee3da048442ff00e518461e456c81234f
aschaefer <andre.schaefer@namics.com>
2021-09-21 10:55:52 +0200

    metzgete

---
+++ anlass/metzgete21.php
@@ -0,0 +1,40 @@
+<?php include '../includes/head.php'; ?>
+<div class="row">
+	<div class="col-md-2 info">
+		<img src="/images/noerdlo-logo.png">
+	</div>
+	<div class="col-md-6 info">
+		<h1>
+			Nördli Metzgete
+			<br/> <small>22/23. Oktober 2021</small>
+		</h1>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-8">
+		Liebe Kameraden und Freunde
+		<p>
+			Es ist wieder soweit. Auch dieses Jahr laden wir alle RC-Kameraden, Freunde und Bekannte zur traditionellen
+			Nördli Metzgete ein. Geniesst einen schönen Abend in unserem Vereinsheim.
+		</p>
+		<p>
+			<strong>
+				Jedermann ist herzlich eingeladen!
+			</strong>
+		</p>
+		<p>
+			Die <q>Nördlimannschaft</q> bewirtet Euch gerne am Freitag, den 22.10.2021 und am Samstag, den 23.10.2021.
+		</p>
+		<p>
+			Wir freuen uns über Eure Anmeldungen bis am 17. Oktober 2021.
+		</p>
+		<ul>
+			<li>E-Mail: noerdli@gmx.ch</li>
+			<li>Telefon: 078 710 30 71</li>
+		</ul>
+		<p>
+			Im Nördli bestehen auch genügend Übernachtungsmöglichkeiten!
+		</p>
+	</div>
+</div>
+<?php include '../includes/foot.php'; ?>
