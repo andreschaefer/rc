504df4791677fc286ff479917ec4cff317cbde78
aschaefer <andre.schaefer@namics.com>
2021-10-03 17:13:56 +0200

    absage

--- anlass/metzgete21.php
+++ anlass/metzgete21.php
@@ -5,8 +5,8 @@
 	</div>
 	<div class="col-md-6 info">
 		<h1>
-			Nördli Metzgete
-			<br/> <small>22/23. Oktober 2021</small>
+			Nördli Metzgete 2021
+			<br/> <small>Abgesagt</small>
 		</h1>
 	</div>
 </div>
@@ -14,27 +14,16 @@
 	<div class="col-md-8">
 		Liebe Kameraden und Freunde
 		<p>
-			Es ist wieder soweit. Auch dieses Jahr laden wir alle RC-Kameraden, Freunde und Bekannte zur traditionellen
-			Nördli Metzgete ein. Geniesst einen schönen Abend in unserem Vereinsheim.
+			Infolge mangelnder Anmeldungen aufgrund der Vorgaben BAG, müssen wir Euch schweren Herzens mitteilen, dass
+			wir die diesjährige Metzgete vom 22. - 23.10.2021 absagen.
 		</p>
 		<p>
-			<strong>
-				Jedermann ist herzlich eingeladen!
-			</strong>
+			Wir hoffen aber sehr darauf, Euch im nächsten Jahr wieder an den Anlässen begrüssen zu dürfen.
 		</p>
-		<p>
-			Die diesjährige Metzgete findet am Fr. 22.10. - Sa. 23.10.2021 im Nördli unter den aktuell geltenden 3G sowie der Zertifikatregeln des BAG statt.
-			Anmeldeschluss ist der Do. 30.9.2021.
 
-			Bei zuwenig Anmeldungen behält sich die Hüttencrew eine Absage der Metzgete vor.
-		</p>
-		<ul>
-			<li>E-Mail: Huettenchef@vereinsheim-noerdli.ch</li>
-			<li>Telefon: 078 710 30 71</li>
-			<lI>Internet: <a href="https://www.vereinsheim-noerdli.ch/">Nördli-Homepage</a> </lI>
-		</ul>
 		<p>
-			Im Nördli bestehen auch genügend Übernachtungsmöglichkeiten!
+			Liebe Grüsse
+			<br> Eurer Nördli-Hüttenteam
 		</p>
 	</div>
 </div>
