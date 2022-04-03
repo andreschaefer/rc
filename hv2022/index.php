95013d96e5111895a023412e13e98be620919de7
aschaefer <andre.schaefer@namics.com>
2022-04-03 10:22:20 +0200

    unterlagen hv2022

---
+++ hv2022/index.php
@@ -0,0 +1,9 @@
+<?php include '../includes/head.php'; ?>
+<main class="hv2020">
+	<h1>Ordentliche Hauptversammlung 2022<br>
+		<h2>Unterlagen</h2>
+		<ul>
+			<li><a href="protokoll-hv2021.pdf">Protokoll der HV 2021</a></li>
+		</ul>
+</main>
+<?php include '../includes/foot.php'; ?>
