5166bf9fd98c94d07b8304a403f786b27f13edb9
aschaefer <andre.schaefer@namics.com>
2022-04-05 17:40:10 +0200

    unterlagen hv2022

---
+++ hv/lr4iU3O3fFZNndV0xd8S/index.php
@@ -0,0 +1,17 @@
+<?php include '../includes/head.php'; ?>
+<main class="hv2020">
+	<h1>Ordentliche Hauptversammlung 2022<br>
+		<h2>Unterlagen</h2>
+		<ul>
+			<li><a href="protokoll-hv2021.pdf">Protokoll der HV 2021</a></li>
+			<li>Antrag Statuten-Änderung
+				<ul>
+					<li><a href="Antrag_Statuten_2022.pdf">Antrag zur Anpassung</a></li></li>
+					<li><a href="Statuten_2022.pdf">neue Statuten 2022</a></li>
+				</ul>
+			</li>
+			<li><a href="programm2022.docx">Programm 2022</a></li>
+			<li><a href="GPK_Bericht.pdf">Bericht der GPK</a></li>
+		</ul>
+</main>
+<?php include '../includes/foot.php'; ?>
