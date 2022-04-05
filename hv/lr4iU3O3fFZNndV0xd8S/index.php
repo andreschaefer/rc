40b158e8722a7993a59cc95c2faea0a0583972fb
aschaefer <andre.schaefer@namics.com>
2022-04-05 17:50:18 +0200

    einladung

--- hv/lr4iU3O3fFZNndV0xd8S/index.php
+++ hv/lr4iU3O3fFZNndV0xd8S/index.php
@@ -3,7 +3,9 @@
 	<h1>Ordentliche Hauptversammlung 2022<br>
 		<h2>Unterlagen</h2>
 		<ul>
+			<li><a href="2022_HV_Einladung.pdf">Einladung zur HV 2022</a></li>
 			<li><a href="protokoll-hv2021.pdf">Protokoll der HV 2021</a></li>
+			<li><a href="GPK_Bericht.pdf">Bericht der GPK</a></li>
 			<li>Antrag Statuten-Änderung
 				<ul>
 					<li><a href="Antrag_Statuten_2022.pdf">Antrag zur Anpassung</a></li></li>
@@ -11,7 +13,6 @@
 				</ul>
 			</li>
 			<li><a href="programm2022.docx">Programm 2022</a></li>
-			<li><a href="GPK_Bericht.pdf">Bericht der GPK</a></li>
 		</ul>
 </main>
 <?php include '../includes/foot.php'; ?>
