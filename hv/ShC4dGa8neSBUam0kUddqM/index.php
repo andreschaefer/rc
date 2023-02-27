31555087caa5bb2a93c277ed1958cf51546361bc
aschaefer <andre.schaefer@namics.com>
2023-02-27 16:52:25 +0100

    HV 2023

---
+++ hv/ShC4dGa8neSBUam0kUddqM/index.php
@@ -0,0 +1,17 @@
+<?php include '../../includes/head-no-index.php'; ?>
+<main class="hv2020">
+	<h1>Ordentliche Hauptversammlung 2023<br>
+		<h2>Unterlagen</h2>
+		<ul>
+			<li><a href="2023_HV_Einladung.pdf">Einladung zur HV 2023</a></li>
+			<li><a href="2022_HV_Protokoll.pdf">Protokoll der HV 2022</a></li>
+			<li><a href="GPK_Bericht.pdf">Bericht der GPK</a></li>
+			<li><a href="programm2023.docx">Programm 2023</a></li>
+		</ul>
+
+		<h2>Anmeldung</h2>
+
+		<a href="https://forms.gle/Bp8AWVBQMCL8sJv27">Direkt Online</a>
+
+</main>
+<?php include '../includes/foot.php'; ?>
